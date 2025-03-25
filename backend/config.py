5bb8fd464e82b54ae0b9fdc355cc2ecf27b4ed55
Dan Roca <DanielRoca101@gmail.com>
2025-03-25 18:11:06 -0400

    Delete backend directory

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'  # Your MySQL username
-    MYSQL_PASSWORD = 'Lycanroc123!'  # Your MySQL password
-    MYSQL_DB = 'study_planner'  # Database name you created earlier
