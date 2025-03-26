4ddb3db801370d6a002ecd9b7427297ebf74daea
Daniel Roca <danielroca101@gmail.com>
2025-03-26 17:37:54 -0400

    Worked for 4 weeks on this on crunch mode

---
+++ backend/config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'  # Your MySQL username
+    MYSQL_PASSWORD = 'Lycanroc123!'  # Your MySQL password
+    MYSQL_DB = 'study_planner'  # Database name you created earlier
